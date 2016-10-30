60577adeed04694b46bc6870b5fe2abc4ab9e392
accumulator <m18636895879@163.com>
2016-10-30 18:07:49 +0800

    new

--- src/main/java/z1f/webServices/CountryEndpoint.java
+++ src/main/java/z1f/webServices/CountryEndpoint.java
@@ -6,12 +6,14 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import z1f.repositories.CountryRepository;
+
 import z1f.models.GetCountryRequest;
 import z1f.models.GetCountryResponse;
 
 @Endpoint
 public class CountryEndpoint {
-	private static final String NAMESPACE_URI = "http://z1d/models";
+	private static final String NAMESPACE_URI = "http://z1f/models";
 
 	private CountryRepository countryRepository;
 
