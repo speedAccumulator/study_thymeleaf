6e78b8580bacf5837f6523a9e84c8b5aa14aadc2
accumulator <m18636895879@163.com>
2016-10-30 18:00:58 +0800

    new

---
+++ src/main/java/z1f/Application.java
@@ -0,0 +1,12 @@
+package z1f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/z1f/webServices/CountryEndpoint.java
@@ -0,0 +1,31 @@
+package z1f.webServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import z1f.models.GetCountryRequest;
+import z1f.models.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://z1d/models";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
